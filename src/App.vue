29f848ee402aa1b3ecc4e771afc9eab6c2f24674
Mohamed Emad <mohamed.elsherbiny215@gmail.com>
2022-06-05 13:13:50 +0200

    add app

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,101 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted, computed, watch } from 'vue'
+
+const todos = ref([])
+const name = ref('')
+
+const inputContent = ref('')
+const inputCategory = ref(null)
+
+const todosAsc = computed(() => todos.value.sort((a, b) => b.createdAt - a.createdAt))
+
+const addTodo = () => {
+  if (inputContent.value.trim() === '' || inputCategory.value === null) return
+
+  todos.value.push({
+    content: inputContent.value,
+    category: inputCategory.value,
+    done: false,
+    createdAt: new Date().getTime()
+  })
+  inputContent.value = ''
+  inputCategory.value = null
+}
+
+const deleteTodo = (todo) => {
+  todos.value = todos.value.filter(t => t !== todo)
+}
+
+watch(todos, (newTodos) => {
+  localStorage.setItem('todos', JSON.stringify(newTodos))
+}, { deep: true })
+
+watch(name, (newName) => {
+  localStorage.setItem('name', newName)
+})
+
+onMounted(() => {
+  name.value = localStorage.getItem('name') || ''
+  todos.value = JSON.parse(localStorage.getItem('todos')) || []
+})
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-</template>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="text" placeholder="Name here" v-model="name" />
+      </h2>
+    </section>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    <section class="create-todo">
+      <h3>Create a TODO</h3>
+
+      <form @submit.prevent="addTodo">
+        <h4>What's on your todo list?</h4>
+        <input type="text" placeholder="e.g. study for course" v-model="inputContent" />
+
+        <h4>Pick a category</h4>
+        <div class="options">
+
+          <label>
+            <input type="radio" name="category" value="work" v-model="inputCategory" />
+            <span class="bubble work"></span>
+            Work
+          </label>
+
+          <label>
+            <input type="radio" name="category" value="personal" v-model="inputCategory" />
+            <span class="bubble personal"></span>
+            Personal
+          </label>
+        </div>
+
+        <input type="submit" value="Add todo" />
+      </form>
+    </section>
+
+    <section class="todo-list">
+      <h3>TODO LIST</h3>
+      <div class="list" id="todo-list">
+
+        <div v-for="(todo, index) in todosAsc" :key="index" :class="`todo-item ${todo.done && 'done'}`">
+          <label>
+            <input type="checkbox" v-model="todo.done" />
+            <span :class="`bubble ${todo.category}`"></span>
+          </label>
+
+          <div class="todo-content">
+            <input type="text" v-model="todo.content" />
+          </div>
+
+          <div class="actions">
+            <button class="delete" @click="deleteTodo(todo)">Delete</button>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+  </main>
+</template>
